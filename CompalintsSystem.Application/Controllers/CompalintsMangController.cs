4dfe3520b52157da22f247aea30c917301fcae4c
Abdulrahman-Aldoais <103898762+Abdulrahman-Aldoais@users.noreply.github.com>
2024-01-06 23:26:28 +0300

    new

---
+++ CompalintsSystem.Application/Controllers/CompalintsMangController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompalintsSystem.Application.Controllers
+{
+    public class CompalintsMangController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
